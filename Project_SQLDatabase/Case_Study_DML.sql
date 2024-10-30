7733b5068091a1f5c13e34979ad3dd53e69a4b14
Joshua Verbiano Inkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2024-10-30 14:34:41 +0700

    Update Case_Study_DML.sql
    Adding Create Table Statement

--- Project_SQLDatabase/Case_Study_DML.sql
+++ Project_SQLDatabase/Case_Study_DML.sql
@@ -1,3 +1,13 @@
+-- Building a table for Study_Case#2
+CREATE TABLE "passangers" (
+	nama_depan	VARCHAR(20),
+	nama_belakang	VARCHAR(20),
+	riwayat_penyakit VARCHAR(30), 
+  umur INT, 
+  departure CHAR, 
+  arrival CHAR
+);
+
 INSERT INTO passangers
 VALUES ('Sri', 'Rahma', 'Vertigo', 20, 'Jakarta', 'Bali'),
 ('Albila', 'Subagja', 'Tidak Ada', 13, 'Jakarta', 'Surabaya'),
