b7f6a0a5701f202076a8ed111361457bfefcb66e
JoshInkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2024-10-30 14:32:01 +0700

    Create Case_Study_DML.sql

---
+++ Project_SQLDatabase/Case_Study_DML.sql
@@ -0,0 +1,12 @@
+INSERT INTO passangers
+VALUES ('Sri', 'Rahma', 'Vertigo', 20, 'Jakarta', 'Bali'),
+('Albila', 'Subagja', 'Tidak Ada', 13, 'Jakarta', 'Surabaya'),
+('Banu', 'Wiranata', 'Maag', 24, 'Surabaya', 'Bali'),
+('Yudistira', 'Prawira', 'Tidak Ada', 10, 'Jakarta', 'Bali'),
+('Wiguna', 'Adi', 'Bronkitis', 19, 'Surabaya', 'Bali'),
+('Triwijaya', 'Galuh', 'Tidak Ada', 17, 'Jakarta', 'Surabaya');
+
+INSERT INTO passangers
+VALUES ('Fransisca', 'Prahastuti', 'Tidak Ada', 31, 'Surabaya', 'Bali');
+
+SELECT * FROM passangers;
